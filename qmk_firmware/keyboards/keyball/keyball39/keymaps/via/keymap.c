6c36c810574af46c5c1b249514564c4d14934853
TBtomoya <suwa.tomoya.ed@gmail.com>
2026-04-09 01:36:34 +0900

    layer fix2

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -63,10 +63,18 @@ void pointing_device_init_user(void) {
 //    return state;
 //}
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    if (get_highest_layer(remove_auto_mouse_layer(state, true)) == 3) {
+        keyball_set_scroll_mode(true);
+    } else {
+        keyball_set_scroll_mode(false);
+    }
     return state;
 }
+//layer_state_t layer_state_set_user(layer_state_t state) {
+//    // Auto enable scroll mode when the highest layer is 3
+//    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+//    return state;
+//}
 
 #ifdef OLED_ENABLE
 
