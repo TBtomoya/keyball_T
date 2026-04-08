a3811f8230f8824924f1013e5b050663f7cb038d
TBtomoya <suwa.tomoya.ed@gmail.com>
2026-04-09 00:44:00 +0900

    layer fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -59,7 +59,7 @@ void pointing_device_init_user(void) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    keyball_set_scroll_mode(layer_state_cmp(state, 4));
+    keyball_set_scroll_mode(layer_state_cmp(state, 3));
     return state;
 }
 //layer_state_t layer_state_set_user(layer_state_t state) {
